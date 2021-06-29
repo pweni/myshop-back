c015c891ea43e0ee09b9275bdc71691744f40dfd
xjboy2018 <5794125@qq.com>
2021-06-29 19:13:40 +0800

    update

--- apps/member/urls.py
+++ apps/member/urls.py
@@ -2,7 +2,9 @@ from django.urls import path,include
 from apps.member import views
 
 urlpatterns = [
-    path('index',views.index),
+    path('index/',views.index),
     path('index_page',views.index_page),
-    path("list",views.list)
+    path("list",views.list),
+    path("index_bttable/",views.index_bttable),
+    path("ajax_member/",views.ajax_member),
 ]
