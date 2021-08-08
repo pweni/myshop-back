7c08b2a0fa22148829fb5a4b1962522a5716e877
xjboy2018 <5794125@qq.com>
2021-08-09 00:18:35 +0800

    1

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 from apps.users.models import *
 @admin.register(MyUser)
 class MyUserAdmin(admin.ModelAdmin):
-    list_display=['username','email','is_active','nickname','mobile']
+    list_display=['username','email','is_active','truename','mobile']
