04492ccb006fcdcaa19a96f355b5921dc694825c
xjboy2018 <5794125@qq.com>
2021-09-16 22:34:47 +0800

    000

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -12,5 +12,12 @@ urlpatterns = [
     path('diy_login/',views.diy_loginView),
     path('userbaseinfo_add',views.userbaseinfo_add),
 
-
+    path('index/',views.index),
+    path('add/',views.add),
+    path('edit/<id>/',views.edit),
+    path('del/<id>/',views.delete),
+    path('index_page',views.index_page),
+    path("list",views.list),
+    path("index_bttable/",views.index_bttable),
+    path("ajax_member/",views.ajax_member),
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -3,6 +3,132 @@ from django.http import HttpResponse, JsonResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import login,logout,authenticate
 from apps.users.models import MyUser
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import F
+
+def add(request):
+    return render(request,'shop/users/add.html')
+
+def edit(request,id):
+    print(id)
+    return render(request,'shop/users/edit.html')
+
+def delete(request,id):
+    obj=MyUser.objects.get(users=id)
+    obj.delete()
+    json_dict={}
+    json_dict["code"]=200
+    json_dict["msg"]="删除数据成功"
+    return JsonResponse(json_dict)
+
+def index(request):
+    if request.method=="GET":
+        level=request.GET.get("level")
+        truename=request.GET.get("truename",'')
+        status=request.GET.get("status")
+
+        search_dict=dict()
+        if level:
+            search_dict["level"]=level
+        if truename: 
+            search_dict["truename"]=truename
+        if status:
+            search_dict["status"]=status
+        
+        datas=MyUser.objects.filter(**search_dict).order_by("-id")
+
+        page_size=2 #每页显示的行数
+        try:
+            if not request.GET.get("page"):
+                curr_page=1
+            curr_page=int(request.GET.get("page"))
+        except:
+            curr_page=1
+
+        paginator=Paginator(datas,page_size)
+        try:
+            users=paginator.page(curr_page)
+        except PageNotAnInteger:
+            users=paginator.page(1)
+        except EmptyPage:
+            users=paginator.page(1)
+        context={
+            'level':level,
+            'truename':truename,
+            'status':status,
+            'users':users,
+        }
+    return render(request,'shop/users/index.html',context=context)
+#改进分页
+def index_page(request):
+    if request.method=="GET":
+        page_size=100 #每页3条数据
+        #page_size=int(request.GET["page_size"])
+        try:
+            if not request.GET["page"]:
+                curr_page=1
+            curr_page=int(request.GET["page"])
+        except:
+            curr_page=1
+        #获取总数count
+        total=MyUser.objects.count()
+        #计算总页数
+        total_page,remainder=divmod(total,page_size)
+        if remainder:
+            total_page+=1
+
+        #通过切片获取当前页和下一页的数据
+        users=MyUser.objects.order_by("-id")[(curr_page-1)*page_size:curr_page*page_size]
+        return render(request,'users/index_page.html',{"users":users,"total_page":range(1,total_page+1),"curr_page":curr_page})
+    
+def get_memberinfo(request):
+    if request.method=="GET":
+        page_size=3
+        curr_page=int(request.GET["page"])
+        #获取总数count
+        total=models.MyUser.objects.count()
+        #通过切片获取当前页和下一页的数据
+        users=models.MyUser.objects.order_by("-id")[(page_num-1)*page_size:page_num*page_size]
+        rows=[]
+        datas={"total":total,"rows":rows}
+        for user in users:
+            rows.append({
+                "id":user.id,
+                "username":user.username,
+                "truename":user.treuname,
+            })
+        return HttpResponse(json.dumps(datas),content_type="application/json")
+
+
+def list(request):
+    return render(request,'users/list.html')
+
+def index_bttable(request):
+    return render(request,'shop/users/index_bttable.html')
+
+def ajax_member(request):
+    total=MyUser.objects.count()
+    users=MyUser.objects.order_by("-id")
+    rows=[]
+    datas={"total":total,"rows":rows}
+    for user in users:
+        rows.append({
+            "id":user.id,
+            "username":user.username,
+            "truename":user.truename,
+            "sex":user.sex,
+            "email":user.email,
+            })
+    return JsonResponse(datas,safe=False,json_dumps_params={'ensure_ascii':False,"indent":4})
+
+
+def index(request):
+    if request.method=="GET":
+        users =MyUser.objects.all().order_by("-id")
+        context={
+            'users':users,
+        }
+    return render(request,'shop/users/index.html',context=context)
 
 
 def user_reg(request):
