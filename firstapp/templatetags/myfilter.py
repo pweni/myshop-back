1d4cd5ddf7a792dace32310ac10ba1f8ee197dcb
Administrator <65350091_xj>
2022-02-06 23:10:11 +0800

    del

--- firstapp/templatetags/myfilter.py
+++
@@ -1,9 +0,0 @@
-from django import template
-register = template.Library()
-
-@register.filter
-def show_title(value,n):
-    if value.__len__() > n:
-        return f'{value[0:n]}...'
-    else:
-        return value
